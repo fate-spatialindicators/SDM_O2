917b5283a834d46736b8dfc331fa19a16be78055
Tim Essington <tessington@users.noreply.github.com>
2021-05-10 14:17:12 -0700

    really add correct mi_functions

--- code/mi_functions.R
+++ code/mi_functions.R
@@ -85,25 +85,121 @@ load_all_hauls <- function() {
    
 }
 
-load_data <- function() {
-  dat = readRDS("survey_data/joined_nwfsc_data.rds")
+calc_po2_mi <- function(dat) {
+  #O2 from trawl data is in ml/l - may need to be converted to umol/kg
+  gas_const = 8.31
+  partial_molar_vol = 0.000032
+  kelvin = 273.15
+  boltz = 0.000086173324
+  
+  #calculate percent saturation for O2 - assumes  units of mL O2/L
+  # Input:       S = Salinity (pss-78)
+  #              T = Temp (deg C) ! use potential temp
+  #depth is in meters
+  #[umole/kg] = [ml/L]*44660/(sigmatheta(P=0,theta,S) + 1000)
+  dat$SA = gsw_SA_from_SP(dat$sal,dat$depth,dat$longitude_dd,dat$latitude_dd) #absolute salinity for pot T calc
+  dat$pt = gsw_pt_from_t(dat$SA,dat$temp,dat$depth) #potential temp at a particular depth
+  dat$CT = gsw_CT_from_t(dat$SA,dat$temp,dat$depth) #conservative temp
+  dat$sigma0 = gsw_sigma0(dat$SA,dat$CT)
+  dat$o2_umolkg = dat$o2*44660/(dat$sigma0+1000)
+  
+  
+  dat$O2_Sat0 = gsw_O2sol_SP_pt(dat$sal,dat$pt)
+  
+  #= o2satv2a(sal,pt) #uses practical salinity and potential temp - solubity at p =1 atm
+  dat$press = exp(dat$depth*10000*partial_molar_vol/gas_const/(dat$temp+kelvin))
+  dat$O2_satdepth = dat$O2_Sat0*dat$press
+  
+  #solubility at p=0
+  dat$sol0 = dat$O2_Sat0/0.209
+  dat$sol_Dep = dat$sol0*dat$press
+  dat$po2 = dat$o2_umolkg/dat$sol_Dep
+  
+  # species-specific parameters
+  Ao = 1.16625e-13
+  Eo = 0.8736 # from cod, 0.8736.  Make it one half or double
+  B = 1200 # size in grams, roughly average (initial calculations used 10000g)
+  n = -0.208 # borrowed from cod 
+  
+  dat$mi = B^n*Ao*dat$po2/exp(-1*Eo/(boltz*(dat$temp+kelvin))) 
+  return(dat)
+}
+
+load_data <- function(fit.model= F, spc, constrain_latitude = F) {
+  
+  dat <- readRDS("survey_data/joined_nwfsc_data.rds")
+  
   # analyze sablefish for years and hauls with adequate oxygen and temperature data, within range of occurrence
-  dat = dplyr::filter(dat, species == "sablefish", year%in%seq(2010,2015), 
-               !is.na(temp), !is.na(o2), !is.na(sal),!is.infinite(sal),
-               latitude_dd > min(latitude_dd[which(cpue_kg_km2>0)]),
-               latitude_dd <= max(latitude_dd[which(cpue_kg_km2>0)]),
-               longitude_dd > min(longitude_dd[which(cpue_kg_km2>0)]),
-               longitude_dd < max(longitude_dd[which(cpue_kg_km2>0)]))
+  dat = dplyr::filter(dat, species == spc, year%in%seq(2010,2015))
+  if (constrain_latitude) dat <- dplyr::filter(dat, latitude_dd >=43)
+
   
   
   # get julian day
   dat$julian_day <- rep(NA, nrow(dat))
   for (i in 1:nrow(dat)) dat$julian_day[i] <- as.POSIXlt(dat$date[i], format = "%Y-%b-%d")$yday
 
+  
+  # create temporary data file, matching J-SCOPE extent, for model fitting
+  if(fit.model) {
+    # constraint to J-SCOPE extent
+    # UTM transformation
+    dat_ll = dat
+    coordinates(dat_ll) <- c("longitude_dd", "latitude_dd")
+    proj4string(dat_ll) <- CRS("+proj=longlat +datum=WGS84")
+    # convert to utm with spTransform
+    dat_utm = spTransform(dat_ll, 
+                          CRS("+proj=utm +zone=10 +datum=WGS84 +units=km"))
+    # convert back from sp object to data frame
+    dat$X <- as.data.frame(dat_utm)$longitude_dd
+    dat$Y <- as.data.frame(dat_utm)$latitude_dd
+    # scale depth and julian dat
+    dat$log_depth_scaled <- scale(log(dat$depth))
+    dat$log_depth_scaled2 <- dat$log_depth_scaled^2
+    dat$jday_scaled <- scale(dat$julian_day)
+    dat$jday_scaled2 <- dat$jday_scaled^2
+    # create temporary data file for fitting
+    fit_dat <- dplyr::filter(dat, !is.na(o2))
+    
+    
+    c_spde <-make_mesh(data = fit_dat, xy_cols = c("X", "Y"), n_knots = 250) # choose # knots
+    # fit dissolved oxygen 
+    o2_model <-  sdmTMB(formula = o2 ~ -1 + log_depth_scaled + log_depth_scaled2 
+                        +  as.factor(year) + jday_scaled + jday_scaled2,
+                        data = fit_dat,
+                        time = "year", spde = c_spde, anisotropy = TRUE,
+                        silent = TRUE, spatial_trend = FALSE, spatial_only = FALSE,
+                        control = sdmTMBcontrol(step.min = 0.01, step.max = 1))
+    # get predictions
+    pred_o2 <- predict(o2_model,
+                                 newdata = dat,
+                                 return_tmb_object = F)
+    #impute missing values
+    index <- which(is.na(dat$o2))
+    dat$o2[index] <- pred_o2$est[index]
+    
+    # impute salinity, following same steps
+    sal_model <- sdmTMB(formula = o2 ~ -1 + log_depth_scaled + log_depth_scaled2 
+                         +  as.factor(year) + jday_scaled + jday_scaled2,
+                         data = fit_dat,
+                         time = "year", spde = c_spde, anisotropy = TRUE,
+                         silent = TRUE, spatial_trend = FALSE, spatial_only = FALSE,
+                         control = sdmTMBcontrol(step.min = 0.01, step.max = 1)
+    )
+  pred_sal <- predict(sal_model,
+                          newdat = dat,
+                          return_tmb_object = F)
+  index <- which(is.na(dat$sal))
+  dat$sal[index] <- pred_sal$est[index]
+  }
+  
   # compute metabolic index (mi) --------------------------------------------
   # converted from Halle Berger matlab script
   
-  #O2 from trawl data is in ml/l - may need to be converted to umol/kg
+  #O2 from trawl data is in ml/l 
+  # just in case, remove any missing or nonsense values from sensors
+  dat <- dplyr::filter(dat, !is.na(o2), !is.na(sal), !is.na(temp), is.finite(sal))
+  
   gas_const = 8.31
   partial_molar_vol = 0.000032
   kelvin = 273.15
@@ -132,18 +228,19 @@ load_data <- function() {
   dat$sol_Dep = dat$sol0*dat$press
   dat$po2 = dat$o2_umolkg/dat$sol_Dep
   
-  # species-specific parameters
+  # species-specific parameters, Ao doesn't really matter because it is a constant
   Ao = 1.16625e-13
   Eo = 0.8736 # from cod, 0.8736.  Make it one half or double
+  # the below also doesn't matter because B^n is a constant and the index is scaled for fitting.  
   B = 1200 # size in grams, roughly average (initial calculations used 10000g)
   n = -0.208 # borrowed from cod 
   
   dat$mi = B^n*Ao*dat$po2/exp(-1*Eo/(boltz*(dat$temp+kelvin)))
-  dat$longitude <- dat$longitude / 10
-  dat$latitude <- dat$latitude / 10
+  dat <- dplyr::filter(dat, !is.na(temp), !is.na(mi))
+  
   # prepare data and models -------------------------------------------------
   
-  dat <- dplyr::select(dat, species, year, longitude_dd, latitude_dd, cpue_kg_km2,
+  dat <- dplyr::select(dat, trawl_id, species, year, longitude_dd, latitude_dd, cpue_kg_km2,
                 o2, temp, depth, mi, po2, julian_day, pass)
   
   
@@ -161,10 +258,12 @@ load_data <- function() {
   return(dat)
 }
 
-load_data_jscope <- function(spc) {
+load_data_jscope <- function(spc, years) {
   
   #load trawl data
   hauldat <- readRDS("survey_data/joined_jscope_trawl_2003_2018.rds")
+  
+  hauldat <- dplyr::filter(hauldat, year %in% years)
   catchdat <- readRDS("survey_data/joined_nwfsc_data.rds")
   catchdat <- dplyr::select(catchdat, c(trawl_id, project, year, pass, vessel, tow, date,
                                         longitude_dd, latitude_dd, area_swept_ha, cpue_kg_km2,
@@ -175,10 +274,14 @@ load_data_jscope <- function(spc) {
                            
  
   # analyze sablefish for years and hauls with adequate oxygen and temperature data, within range of occurrence
-  catchdat = dplyr::filter(catchdat, species == spc, year%in%seq(2003,2018))
+  catchdat = dplyr::filter(catchdat, species == spc, year%in%years)
   # merge with new jscope
   dat <- inner_join(catchdat, hauldat, by = "trawl_id")
   dat <- dplyr::rename(dat, o2 = o2_model, temp = temp_model, sal = sal_model)  
+  
+  # remove nonsense rows
+  dat <- dat %>%
+    filter(!is.na(o2), o2>0, !is.na(sal), !is.infinite(sal), !is.na(temp))
   
   # get julian day
   dat$julian_day <- rep(NA, nrow(dat))
@@ -228,7 +331,7 @@ load_data_jscope <- function(spc) {
   dat$latitude <- dat$latitude / 10
   # prepare data and models -------------------------------------------------
   
-  dat <- dplyr::select(dat, species, year, longitude_dd, latitude_dd, cpue_kg_km2,
+  dat <- dplyr::select(dat, species, trawl_id, year, longitude_dd, latitude_dd, cpue_kg_km2,
                        o2, temp, depth, mi, po2, julian_day, pass)
   
   
@@ -331,24 +434,41 @@ plot_map <- function(dat, column = "est") {
     coord_fixed()
 }
 
-get_models <- function() {
+get_models_compare <- function() {
   formula <- c("log_depth_scaled + log_depth_scaled2 + as.factor(year)", 
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp", 
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + o2",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + po2",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + mi",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp + o2",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp + o2 + temp*o2",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp + po2",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp + po2 + temp * po2",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(o2)",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(o2)+ temp",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2)",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2) + temp",
-                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(mi)"
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp_trawl", 
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + po2_trawl",
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + mi_trawl",
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp_trawl + po2_trawl",
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp_trawl + po2_trawl + temp_trawl * po2_trawl",
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2_trawl)",
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2_trawl) + temp_trawl",
+                                      "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(mi_trawl)",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year)", 
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp_jscope", 
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + po2_jscope",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + mi_jscope",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp_jscope + po2_jscope",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp_jscope + po2_jscope + temp_jscope * po2_jscope",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2_jscope)",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2_jscope) + temp_jscope",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(mi_jscope)"
                                       )
 }
-                      
+ 
+
+get_models <- function() {
+  formula <- c("log_depth_scaled + log_depth_scaled2 + as.factor(year)", 
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp", 
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + po2",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + mi",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp + po2",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + temp + po2 + temp * po2",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2)",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(po2) + temp",
+               "log_depth_scaled + log_depth_scaled2 + as.factor(year) + breakpt(mi)"
+  )
+}                     
 get_bp_parameters <- function(m) {
   fixed_effects <- m$model$par
   bp_ind <- grep("b_threshold", names(fixed_effects))
